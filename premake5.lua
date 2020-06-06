a00853ef165fd7b48d871b3d5ea59e1530a4bb6c
Manuel Ott <email@manuelott.com>
2020-06-06 21:07:48 +0200

    updated the PreMake config

--- premake5.lua
+++ premake5.lua
@@ -1,6 +1,7 @@
 project "GLFW"
 	kind "StaticLib"
 	language "C"
+	staticruntime "on"
 
 	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
 	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
@@ -18,10 +19,9 @@ project "GLFW"
 		"src/window.c"
 	}
 	filter "system:linux"
-		pic "On"
+		pic "on"
 
 		systemversion "latest"
-		staticruntime "On"
 
 		files
 		{
@@ -44,7 +44,6 @@ project "GLFW"
 
 	filter "system:windows"
 		systemversion "latest"
-		staticruntime "On"
 
 		files
 		{
